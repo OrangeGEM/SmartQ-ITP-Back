0f1a741446bb6d4c4c2329b76863a20fa5e90221
Vitaliy <vitalij.volkov@itp-it.com>
2021-11-23 22:06:25 +0300

    Add routes and client-server req

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
-
 app.use(express.json({ extended: true }))
 
-const PORT = config.get('port') || 5000;
+app.use(
+    cors({
+      credentials: true,
+      origin: ["http://localhost:3000"],
+      optionsSuccessStatus: 200
+    })
+  );
 
-app.use('api/auth', require('./routes/auth.routes'));
+
+app.use('/api/auth', require('./routes/auth.routes'));
 
 async function start() {
     try{
         await mongoose.connect(config.get('mongoUrl'), {
             useNewUrlParser : true,
             useUnifiedTopology : true
+        }, ()=> {
+            console.log("[MongoDB] -> Initialized");
         });
     } catch (e) {
         console.log('Server Error', e.message);
@@ -22,5 +31,7 @@ async function start() {
     }
 }
 start();
+
+const PORT = config.get('port') || 5000;
 
 app.listen(PORT, ()=> { console.log('App has been started on port ' + PORT) } );
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,18 +1,91 @@
 const {Router} = require('express');
 const router = Router();
-
+const bcrypt = require('bcryptjs');
+const {check, validationResult} = require('express-validator');
+const User = require('../models/User'); //Mongo Model
 
 // api/auth/login
-router.post('/login', async (req, res)=> {
-    console.log('Request body:', req.body)
-    res.status(200).json({message:"All Ok!"});
+router.post('/login', 
+    [
+        check('email', 'Некорректный email'),
+        check('password', 'Минимальная длина пароля 8 символов'),
+    ],
+    async (req, res)=> {
+    
+    try {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(), message: 'Некорректные данные при регистрации'
+            })
+        }
+
+        const {email, password} = req.body;
+
+        const user = await User.findOne({ email }) //Поиск 
+        if(!user) {
+            return res.status(400).json({ message: 'Пользователь не найден' })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password); //Проверка паролей
+        if(!isMatch) {
+            return res.status(400).json({ message: 'Неверный пароль' });
+        }
+
+        res.status(200).json({ message:'Аутентифицирован' }); //Auth - Next
+    } catch(e) {
+        console.log(e);
+        res.status(500).json({ message:e.array() })
+    }
+    
 });
 
 
 // api/auth/register
-router.post('/register', async (req, res)=> {
-    console.log('Request body:', req.body)
-    res.status(200).json({message:"All Ok!"});
+router.post(
+    '/register', 
+    [
+        check('email', 'Некорректный email'),
+        check('password', 'Минимальная длина пароля 8 символов'),
+        check('rpassword', 'Неправильный пароль')
+    ],
+    async (req, res)=> {
+    //console.log('Request body:', req.body)
+    try {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(), message: 'Некорректные данные при регистрации'
+            })
+        }
+
+        const {email, password, rpassword} = req.body; 
+        if(password.length < 6) {
+            if(password != rpassword) { //Проверка на соответствие паролей
+                return res.status(400).json({ message: 'Пароли не соответствуют друг другу' });
+            }
+            return res.status(400).json({ message: 'Пароль меньше 6 символов' });
+        }
+
+        const candidate = await User.findOne({ email }); //Проверка на наличие 
+        if(candidate) {
+            return res.status(400).json({ message: 'Такой пользователь уже существует' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const user = new User({ email, password: hashedPassword });
+
+        await user.save();
+        console.log("[MongoDB] -> User Add")
+        return res.status(201).json({ message:'Пользователь создан' })
+    } catch(e) {
+        console.log(e);
+        return res.status(500).json({ message:e.array() });
+    }
+
+
 });
 
 module.exports = router;
