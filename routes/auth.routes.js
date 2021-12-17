3a470fb4c009d14d1e7eeac1b43259bc1426178e
Vitalii <vitalij.volkov@itp-it.com>
2021-12-17 18:48:38 +0300

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -174,7 +174,7 @@ router.post('/verify', async (req, res) => {
                 return res.status(401).json({ message: 'Пользователь не зарегистрирован'  })
             }
 
-            return res.status(200).json({ message: 'Пользователь авторизован', email: user.email });
+            return res.status(200).json({ message: 'Пользователь авторизован', email: user.email, id: user._id });
         })();
     } catch(e) {
         console.log(e);
