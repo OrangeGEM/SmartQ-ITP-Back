35fbf2f2c416427bc234334ceaaee4fe7560dd3f
Vitalii <vitalij.volkov@itp-it.com>
2021-12-21 17:50:47 +0300

    Add delete queue/member

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -12,13 +12,40 @@ const QueueModel = require('../models/Queue')
 router.post('/getqueue', async (req, res) => {
     try{
         const queuesUsers = await QueueModel.find({ user_id: req.body.userId })
-        console.log('Queues: ', queuesUsers);  
+        //console.log('Queues: ', queuesUsers);  
         return res.status(200).json(queuesUsers);
     } catch(e) {
         console.log(e)
         return res.status(500).json({ message: e }); 
     }
 })
+
+//api/profile/deletequeue
+router.post('/deletequeue', async (req, res) => {
+    try{
+        const queuesUsers = await QueueModel.find({ user_id: req.body.userId })
+        //console.log('Queues: ', queuesUsers);  
+        return res.status(200).json(queuesUsers);
+    } catch(e) {
+        console.log(e)
+        return res.status(500).json({ message: e }); 
+    }
+})
+
+//api/profile/deletemember
+router.post('/deletemember', async (req, res) => {
+    try{
+        const filter = {_id: req.body.queueId}
+        const update = {units: [...req.body.members]}
+
+        const queue = await QueueModel.findOneAndUpdate(filter, update)
+        return res.status(200).json({ message: 'Ok, member delete' });
+    } catch(e) {
+        console.log(e)
+        return res.status(500).json({ message: e }); 
+    }
+})
+
 
 
 module.exports = router;
