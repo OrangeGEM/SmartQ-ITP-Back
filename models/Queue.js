68c3da65f7b610ed8b7e85c5977ac3bf82c1b5ad
Vitaliy <vitalij.volkov@itp-it.com>
2021-12-27 16:46:44 +0300

    refactor

--- models/Queue.js
+++ models/Queue.js
@@ -1,18 +1,11 @@
 const {Schema, model, Types} = require('mongoose');
 
-const locale = 'en';
-const today = new Date();
-    
-const month = today.toLocaleDateString(locale, { month: "short" })
-const day = today.toLocaleDateString(locale, { day: "numeric" })
-const year = today.toLocaleDateString(locale, { year: "numeric" })
-
 const QueueSchema = new Schema({
     "user_id" : { type: String, required: true },
     "title" : { type: String },
     "keyword" : { type: String, required: true },
     "description" : { type: String },
-    "date" : { type: String, default: `${month}. ${day} ${year}`},
+    "date" : { type: String },
     "wrap" : { type: Boolean, default: false },     
     "units" : [],
     "ticketNum" : { type: Number }
