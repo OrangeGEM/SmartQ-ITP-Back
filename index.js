0dd62e06e8f302d4839f3c0009b23d2af6e46d43
Vitalii <vitalij.volkov@itp-it.com>
2022-01-06 14:35:15 +0300

    Revert "Create socketio routing for realtime count"
    This reverts commit aad14f5d9aca77c272a31db409dba69e696e30b3.

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const socketIO = require('socket.io');
 const fileUpload = require('express-fileupload');
 const app = express();
 const server = http.createServer(app);
-const QueueModel = require('./models/Queue')
 const io = socketIO(server, {
     cors: {
         origin: "http://localhost:3000",
@@ -30,16 +29,14 @@ app.use('/api/profile', require('./routes/profile.routes'));
 app.use('/api/landing', require('./routes/landing.routes'));
 app.use('/api/files', require('./routes/files.routes'));
 
-let queueCount = 0;
+
 async function start() {
     try{
         await mongoose.connect(config.get('mongoUrl'), {
             useNewUrlParser : true,
             useUnifiedTopology : true
-        }, async ()=> {
+        }, ()=> {
             console.log("[MongoDB] -> Initialized");
-            queueCount = await (await QueueModel.find({})).length;
-            console.log("[MongoDB] -> Queue count: ", queueCount)
         });
     } catch (e) {
         console.log('Server Error', e.message);
@@ -51,23 +48,8 @@ start();
 io.on('connection', (socket) => {
     console.log('[SocketIO] -> Client connected with id: ', socket.id)
 
-    socket.on('changeQueueCount', (data) => {
-        console.log('[SocketIO] -> Emited from client (changeQueueCount)')
-        queueCount += data;
-        socket.broadcast.emit('changeQueueCount', queueCount);
-    })
-
-    socket.on('getQueueCount', () => { 
-        //console.log(`[SocketIO] -> Emited from client (getQueueCount : ${queueCount})`)
-        socket.emit('getQueueCount', queueCount)
-    })
-    
     socket.on('disconnect', (reason) => {
         console.log('[SocketIO] -> Client disconnected with reason: ', reason)
-    })
-
-    socket.on('connect_error', (err) => {
-        console.log(`connect_error due to ${err.message}`);
     })
 })
 
